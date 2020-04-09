215f32d4190be862feaa265757058f8e32d264a0
ZhangJiangKun <1723442668@qq.com>
2020-04-09 17:46:14 +0800

    add homework

---
+++ 20200409/Homework.java
@@ -0,0 +1,26 @@
+public class Homework {
+
+    public static void main(String[] args) {
+        int a = 6;
+        String str = String.valueOf(a);
+        System.out.println(str);
+
+        String s = "1234";
+        int n = Integer.parseInt(s);
+        System.out.println(n);
+    }
+
+    public static void main1(String[] args) {
+        short a = 128;
+        byte b = (byte)a;
+        System.out.println(b);
+
+        double x=2.0; 
+        int y=4; 
+        x/=++y; 
+        System.out.println(x);
+
+        // byte aa = 130;
+
+    }
+}
